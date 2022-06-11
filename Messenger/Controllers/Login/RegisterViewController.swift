433c651af24edfecb16fdf2a78ff86277ecf6b73
annashi4 <ann.shirinkina@gmail.com>
2022-06-12 00:06:36 +0400

    Register view

--- Messenger/Controllers/Login/RegisterViewController.swift
+++ Messenger/Controllers/Login/RegisterViewController.swift
@@ -7,23 +7,159 @@
 
 import UIKit
 
-class RegisterViewController: UIViewController {
-
+class RegisterViewController: UIViewController, UITextFieldDelegate {
+    
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        
+        return scrollView
+    }()
+    
+    private var emailField: UITextField = {
+        let field = UITextField()
+        field.autocorrectionType = .no
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        
+        field.leftView = UIView(frame: CGRect(x: 0,
+                                              y: 0,
+                                              width: 5,
+                                              height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        
+        field.placeholder = "Email Adress..."
+        
+        return field
+    }()
+    
+    private var passwordField: UITextField = {
+        let field = UITextField()
+        field.autocorrectionType = .no
+        field.autocorrectionType = .no
+        field.returnKeyType = .done
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        
+        field.leftView = UIView(frame: CGRect(x: 0,
+                                              y: 0,
+                                              width: 5,
+                                              height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        field.isSecureTextEntry = true
+        
+        field.placeholder = "Password"
+        
+        return field
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .link
+        button.setTitle("Log in", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        
+        return button
+    }()
+    
+    private let imageView: UIView = {
+        
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register",
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(didTapRegister))
+        loginButton.addTarget(self,
+                              action: #selector (loginButtonTapped),
+                              for: .touchUpInside)
+        
+        emailField.delegate = self
+        passwordField.delegate = self
+        
+        //Subviews
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(emailField)
+        scrollView.addSubview(passwordField)
+        scrollView.addSubview(loginButton)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.frame = view.bounds
+        let size = scrollView.width/3
+        imageView.frame = CGRect(x: (scrollView.width - size)/2,
+                                 y: 20,
+                                 width: size,
+                                 height: size)
+        emailField.frame = CGRect(x:30,
+                                  y: imageView.bottom + 10,
+                                  width: scrollView.width - 60,
+                                  height: 52)
+        passwordField.frame = CGRect(x:30,
+                                     y: emailField.bottom + 10,
+                                     width: scrollView.width - 60,
+                                     height: 52)
+        loginButton.frame = CGRect(x:30,
+                                   y: passwordField.bottom + 10,
+                                   width: scrollView.width - 60,
+                                   height: 52)
     }
-    */
-
+    
+    @objc private func loginButtonTapped(){
+        guard let email = emailField.text, let password = passwordField.text,
+              !email.isEmpty, !password.isEmpty, password.count >= 6 else {
+            return
+        }
+        
+    }
+    
+    func alertUserLoginError(){
+        let alert = UIAlertController(title: "Woops", message: "Please enter all information", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+        
+        present (alert, animated: true)
+    }
+    
+    @objc private func didTapRegister() {
+        
+        let registerVC = RegisterViewController()
+        registerVC.title = "Create Account"
+        navigationController?.pushViewController(registerVC, animated: true)
+    }
+    
+}
+// it's supposed to have UITextFieldDelegate but adding it returns an error. CHECK LATER
+extension RegisterViewController {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField == emailField {
+            passwordField.becomeFirstResponder()
+        }
+        else if textField == passwordField {
+            
+            loginButtonTapped()
+        }
+        
+        return true
+    }
+    
 }
